e09283a72f7d5cd75f75257ec84dc1f0cc2bac40
AchmadWibawa <achmadwibawa64@gmail.com>
2022-02-27 18:32:36 +0700

    SingleActionController

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return "Achmad Wibawa, 2041720174";
+    }
+}
